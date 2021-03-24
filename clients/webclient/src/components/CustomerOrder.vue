13ecdf71392680774f24af8dc3218bec7a97e0ec
h577499 <toril.sunde.apelthun@gmail.com>
2021-03-24 22:07:58 +0100

    Added an empty CustomerOrder component

---
+++ clients/webclient/src/components/CustomerOrder.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-container>
+		<v-row class="text-center">
+			<v-col cols="12">
+				<h3>Dette er CustomerOrder-komponenten</h3>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class CustomerOrder extends Vue {
+    
+}
+</script>
