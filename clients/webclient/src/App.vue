5fbb9f639f8f4e3217d43fcb35dfae17a9c2047a
Synnøve Sørensen <578010@stud.hvl.no>
2021-03-24 21:16:06 +0100

    Added new title and icon in app bar

--- clients/webclient/src/App.vue
+++ clients/webclient/src/App.vue
@@ -6,23 +6,8 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-icon class="pr-8" x-large color="white">mdi-bike</v-icon>
+        <v-toolbar-title>Lunsj på midlertidige hjul</v-toolbar-title>
       </div>
       <v-spacer></v-spacer>
     </v-app-bar>
