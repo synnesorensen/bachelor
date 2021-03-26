ecdcd5be4e025438fcc849e11ae025f628a9325c
Synnøve Sørensen <578010@stud.hvl.no>
2021-03-26 13:05:03 +0100

    Added method to add userprofile

--- server/src/userprofile.ts
+++ server/src/userprofile.ts
@@ -25,28 +25,28 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
 
 export const mainHandler = middy(handler).use(cors());
 
-let customerId = "synne@birthdaygirl.yay";
+let userId = "synne@birthdaygirl.yay";
 // TODO: Fetch customerId from JWT
 
 async function getUserprofile(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
 
     let params = {
         TableName: 'MainTable',
-        KeyConditionExpression: "#pk = :customerId and #sk = :customerId",
+        KeyConditionExpression: "#pk = :userId and #sk = :userId",
         ExpressionAttributeNames: {
             "#pk": "pk",
             "#sk": "sk"
         },
         ExpressionAttributeValues: {
-            ":customerId": "c#" + customerId
+            ":userId": "u#" + userId
         }
     }
     try {
-        let customer = await documentClient.query(params).promise();
+        let userprofile = await documentClient.query(params).promise();
 
         return {
             statusCode: 200,
-            body: JSON.stringify(customer.Items)
+            body: JSON.stringify(userprofile.Items)
         };
     } catch (err) {
         return {
@@ -57,7 +57,69 @@ async function getUserprofile(event: APIGatewayProxyEvent): Promise<APIGatewayPr
 }
 
 async function putUserprofile(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-    throw new Error('Function not implemented.');
+    let body = JSON.parse(event.body);
+
+    let UpdateExpression = "set EntityType = :EntityType";
+    let ExpressionAttributeValues: any = {
+        ":EntityType": { S: "Userprofile" }
+    };
+
+    if (body.firstname != undefined) {
+        UpdateExpression += ", firstname = :firstname",
+            ExpressionAttributeValues[":firstname"] = { S: body.firstname };
+    }
+
+    if (body.lastname != undefined) {
+        UpdateExpression += ", lastname = :lastname",
+            ExpressionAttributeValues[":lastname"] = { S: body.lastname };
+    }
+
+    if (body.address != undefined) {
+        UpdateExpression += ", address = :address",
+            ExpressionAttributeValues[":address"] = { S: body.address };
+    }
+
+    if (body.phone != undefined) {
+        UpdateExpression += ", phone = :phone",
+            ExpressionAttributeValues[":phone"] = { N: body.phone };
+    }
+
+    if (body.phone != undefined) {
+        UpdateExpression += ", email = :email",
+            ExpressionAttributeValues[":email"] = { S: body.email };
+    }
+
+    let params = {
+        TableName: "MainTable",
+        Key: {
+            "pk": { S: "u#" + userId },
+            "sk": { S: "u#" + userId }
+        },
+        UpdateExpression,
+        ExpressionAttributeValues,
+        ReturnValues: "ALL_NEW"
+    };
+
+    try {
+        let dbItem = await database.updateItem(params).promise();
+
+        let userprofile = {
+            firstname: dbItem.Attributes.firstname,
+            lastname: dbItem.Attributes.lastname,
+            address: dbItem.Attributes.address,
+            phone: dbItem.Attributes.phone,
+            email: dbItem.Attributes.email
+        };
+        return {
+            statusCode: 200,
+            body: JSON.stringify(userprofile)
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(err)
+        };
+    }
 }
 
 async function deleteUserprofile(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
@@ -65,8 +127,8 @@ async function deleteUserprofile(event: APIGatewayProxyEvent): Promise<APIGatewa
     let params = {
         TableName: 'MainTable',
         Key: {
-            'pk': { S: 'c#' + customerId },
-            'sk': { S: 'c#' + customerId }
+            'pk': { S: 'u#' + userId },
+            'sk': { S: 'u#' + userId }
         }
     };
 
