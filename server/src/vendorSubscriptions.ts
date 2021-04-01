c4996e35c18b937bc5a22fa21975e333ce012b5f
Synnøve Sørensen <578010@stud.hvl.no>
2021-04-01 14:10:16 +0200

    Added missing try catch

--- server/src/vendorSubscriptions.ts
+++ server/src/vendorSubscriptions.ts
@@ -1,3 +1,4 @@
+import 'source-map-support/register'
 import middy from 'middy';
 import cors from '@middy/http-cors';
 import { APIGatewayProxyResult } from 'aws-lambda'
@@ -5,15 +6,21 @@ import { getVendorSubscriptionsFromDb } from './dbUtils';
 
 async function handler(): Promise<APIGatewayProxyResult> {
     let vendorId = "lunsj@hjul.no";
+    // TODO: Get vendorId from JWT
 
-    let userSubscriptions = await getVendorSubscriptionsFromDb(vendorId);
+    try {
+        let userSubscriptions = await getVendorSubscriptionsFromDb(vendorId);
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(userSubscriptions)
-    };
-
+        return {
+            statusCode: 200,
+            body: JSON.stringify(userSubscriptions)
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(err)
+        }
+    }; 
 }
 
 export const mainHandler = middy(handler).use(cors());
-
