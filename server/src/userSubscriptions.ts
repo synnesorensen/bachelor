9853956c287d6e6fcc034dfe377037336901c41f
Synnøve Sørensen <578010@stud.hvl.no>
2021-04-04 14:03:32 +0200

    Renamed methods to match dbUtils

--- server/src/userSubscriptions.ts
+++ server/src/userSubscriptions.ts
@@ -2,20 +2,21 @@ import 'source-map-support/register'
 import middy from 'middy';
 import cors from '@middy/http-cors';
 import { APIGatewayProxyResult } from 'aws-lambda'
-import { getUserSubscriptionsFromDb } from './dbUtils';
+import { getSubscriptionsForUser } from './dbUtils';
 
 async function handler(): Promise<APIGatewayProxyResult> {
     let userId = "synne@birthdaygirl.yay";
     // TODO: Get userId from JWT
     
     try {
-        let vendorSubscriptions = await getUserSubscriptionsFromDb(userId);
+        let vendorSubscriptions = await getSubscriptionsForUser(userId);
 
         return {
             statusCode: 200,
             body: JSON.stringify(vendorSubscriptions)
         };
     } catch (err) {
+        console.log(err);
         return {
             statusCode: 500,
             body: JSON.stringify(err)
--- server/src/vendorSubscriptions.ts
+++ server/src/vendorSubscriptions.ts
@@ -2,20 +2,21 @@ import 'source-map-support/register'
 import middy from 'middy';
 import cors from '@middy/http-cors';
 import { APIGatewayProxyResult } from 'aws-lambda'
-import { getVendorSubscriptionsFromDb } from './dbUtils';
+import { getSubscriptionsForVendor } from './dbUtils';
 
 async function handler(): Promise<APIGatewayProxyResult> {
     let vendorId = "lunsj@hjul.no";
     // TODO: Get vendorId from JWT
 
     try {
-        let userSubscriptions = await getVendorSubscriptionsFromDb(vendorId);
+        let userSubscriptions = await getSubscriptionsForVendor(vendorId);
 
         return {
             statusCode: 200,
             body: JSON.stringify(userSubscriptions)
         };
     } catch (err) {
+        console.log(err);
         return {
             statusCode: 500,
             body: JSON.stringify(err)
