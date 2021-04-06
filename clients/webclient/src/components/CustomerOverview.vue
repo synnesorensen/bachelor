76a3aeb1cc7dbf6abf99bbeaff02ed1a7751be0f
ihjelle <ihjelle@me.com>
2021-04-06 19:04:10 +0200

    Removed unused code

--- clients/webclient/src/components/CustomerOverview.vue
+++ clients/webclient/src/components/CustomerOverview.vue
@@ -1,12 +1,7 @@
 <template>
-      <v-sheet height="600">
+	<v-sheet height="600">
         <v-calendar
-          ref="calendar"
-          v-model="focus"
           color="primary"
-          :events="events"
-          :event-color="getEventColor"
-          :type="type"
         ></v-calendar>
       </v-sheet>
 </template>
@@ -14,7 +9,6 @@
 <script lang="ts">
   import Vue from 'vue'
   import Component from 'vue-class-component'
-  import {getCustomers} from '../api/getCustomers'
 
 @Component
 export default class Customer extends Vue {
