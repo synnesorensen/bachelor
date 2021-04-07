5d8032684194171993950014ef714097a6b6ecc7
h577499 <toril.sunde.apelthun@gmail.com>
2021-04-08 00:00:29 +0200

    Added router link to buttons

--- clients/webclient/src/components/CustomerNavBar.vue
+++ clients/webclient/src/components/CustomerNavBar.vue
@@ -1,16 +1,30 @@
 <template>
 	<v-header>
-		<v-app-bar app color="primary" dark>
+		<v-app-bar app color="primary" dark class="appbar">
 			<div class="d-flex align-center">
 				<v-icon class="pr-8" x-large color="white">mdi-bike</v-icon>
-				<v-toolbar-title>Lunsj på midlertidige hjul</v-toolbar-title>
+				<v-toolbar-title class="titleName">Lunsj på midlertidige hjul</v-toolbar-title>
 				<v-toolbar-items class="navbar">
 					<!-- TODO: Buttons must be converted into router-link to="" -->
-					<v-btn plain class="navbtn">Brukerprofil</v-btn>
-					<v-btn plain class="navbtn">Oversikt</v-btn>
-					<v-btn plain class="navbtn">Faktura</v-btn>
-					<v-btn plain class="navbtn">Bestilling</v-btn>
-					<v-btn plain class="navbtn">Logg ut</v-btn>
+					<v-spacer></v-spacer>
+                    <router-link>
+						<v-btn text class="navbtn">Brukerprofil</v-btn>
+					</router-link>
+					<router-link>
+						<v-btn text class="navbtn">Oversikt</v-btn>
+					</router-link>
+                    <router-link>
+					<v-btn text class="navbtn">Faktura</v-btn>
+					</router-link>
+                    <router-link>
+						<v-btn text class="navbtn">Bestilling</v-btn>
+					</router-link>
+				</v-toolbar-items>
+                <v-spacer></v-spacer>
+				<v-toolbar-items class="logout">
+					<router-link>
+						<v-btn text class="logoutbtn">Logg ut</v-btn>
+					</router-link>
 				</v-toolbar-items>
 			</div>
 		</v-app-bar>
@@ -20,28 +34,48 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import LoginDialog from './LoginDialog/LoginDialog.vue';
-import CustomerOverview from './CustomerOverview.vue';
-import CustomerOrder from './CustomerOrder.vue';
-import CustomerProfile from './CustomerProfile.vue';
-import CustomerInvoice from './CustomerInvoice.vue';
 
-@Component({
-	components: {
-		LoginDialog,
-		CustomerOverview,
-		CustomerOrder,
-		CustomerProfile,
-		CustomerInvoice,
-		CustomerNavBar,
-	},
-})
+// TODO: Must be implemented in a separate .ts file
+// import CustomerInvoice from './CustomerInvoice.vue';
+// import CustomerProfile from './CustomerProfile.vue';
+// import { createRouter, createWebHistory } from 'vue-router';
+
+// const router = createRouter({
+// 	history: createWebHistory(),
+// 	routes: [
+// 		{ path: './profile', name: 'Customer profile', component: CustomerProfile },
+// 		{ path: './invoice', component: CustomerInvoice },
+// 	],
+// });
+
+@Component
 export default class CustomerNavBar extends Vue {}
 </script>
 
 <style scoped>
+.appbar {
+	width: 100%;
+}
+
+.titleName {
+	width: 30%;
+}
+
+.navbar {
+	width: 50%;
+}
+
 .navbtn {
-	color: white;
-	font-size: 1.2rem;
+	font-size: 1rem;
+}
+
+.logout {
+	display: block;
+	width: 20%;
+}
+
+.logoutbtn {
+	font-size: 1rem;
+	float: right;
 }
 </style>
