c04f13f9b473089dfc704cf63fff9a9f86f4d870
Synnøve Sørensen <578010@stud.hvl.no>
2021-04-08 21:51:09 +0200

    First use of cognito and amplify for logging in

--- clients/webclient/src/components/LoginDialog/TabLogin.vue
+++ clients/webclient/src/components/LoginDialog/TabLogin.vue
@@ -3,12 +3,12 @@
         <v-card-title class="headline">Logg inn til din konto</v-card-title>
         <v-card-text>
             <p>Vennligst oppgi brukernavn og passord</p>
-            <v-text-field label="Brukernavn" required></v-text-field>
-            <v-text-field label="Passord" required></v-text-field>
+            <v-text-field v-model="username" label="Brukernavn" required></v-text-field>
+            <v-text-field v-model="password" label="Passord" required></v-text-field>
         </v-card-text>
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="success">Logg inn</v-btn>
+            <v-btn color="success" @click="login">Logg inn</v-btn>
             <v-btn color="error" @click="cancel()">Avbryt</v-btn>
         </v-card-actions>
 
@@ -19,13 +19,30 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import Auth from "@aws-amplify/auth";
 
 @Component
 
 export default class TabLogin extends Vue {
+    private username = "";
+    private password = "";
 
     cancel() {
         this.$emit("cancel");
+    }
+
+    async login() {
+        const config = {
+            region: "eu-north-1",
+            userPoolId: "eu-north-1_T0CZQQ1dX",
+            userPoolWebClientId: "4c4g5j647lbdm1kn07i54nl425",
+            mandatorySignIn: true,
+            authenticationFlowType: 'USER_SRP_AUTH',
+        };
+        
+        Auth.configure(config);
+        let user = await Auth.signIn(this.username, this.password);
+        console.log(user);
     }
 
 }
