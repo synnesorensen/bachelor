b94c3b2cdb9df4743c23a5a1417e7ecc0bd32c48
Synnøve Sørensen <578010@stud.hvl.no>
2021-04-10 16:57:56 +0200

    Initial code for forgotten password

--- clients/webclient/src/components/LoginDialog/TabPassword.vue
+++ clients/webclient/src/components/LoginDialog/TabPassword.vue
@@ -2,11 +2,54 @@
     <v-container>
         <v-card-title class="headline">Glemt passord?</v-card-title>
         <v-card-text>
-            <v-text-field label="Fyll inn epost-adresse" required></v-text-field>
+            <v-text-field 
+                v-if="!showVerification"
+                v-model="username"
+                label="Fyll inn epost-adresse" 
+                required
+                >
+            </v-text-field>
+            <v-text-field 
+                v-if="showVerification"
+                label="Fyll inn kode fra epost" 
+                required
+                v-model="code"
+                >
+            </v-text-field>
+            <v-text-field 
+                v-if="showVerification"
+                label="Fyll inn nytt passord" 
+                required
+                v-model="password1"
+                :rules="[checkPassword]"
+                >
+            </v-text-field>
+            <v-text-field 
+                v-if="showVerification"
+                label="Gjenta nytt passord" 
+                required
+                v-model="password2"
+                :rules="[checkEqual]"
+                >
+            </v-text-field>
         </v-card-text>
+        <p style="color:red;"> {{errorMsg}}</p>
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="success">Send</v-btn>
+            <v-btn 
+                v-if="!showVerification"
+                color="success" 
+                @click="sendPassword"
+            >
+            Send
+            </v-btn>
+            <v-btn 
+                v-else
+                color="success" 
+                @click="verifyPassword"
+            >
+            Send
+            </v-btn>
             <v-btn color="error" @click="cancel()">Avbryt</v-btn>
         </v-card-actions>
 
@@ -19,11 +62,41 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import getAuth from "./auth";
 
 @Component
 
 export default class TabPassword extends Vue {
+    private username = "";
+    private code = "";
+    private password1 = "";
+    private password2 = "";
+    private Auth = getAuth();
+    private showVerification = false;
+    private errorMsg = "";
     
+    // Rules:
+    checkPassword(pass1: string) {
+        return pass1.length > 6 || "Passord må være minst 6 bokstaver";
+    }
+    checkEqual(pass2: string) {
+        return pass2 === this.password1 || "Passordene er ikke like"
+    }
+
+    sendPassword() {
+        this.Auth.forgotPassword(this.username);
+        this.showVerification = true;
+    }
+
+    async verifyPassword() {
+        try {
+            let forgotten = await this.Auth.forgotPasswordSubmit(this.username, this.code, this.password2);
+            console.dir(forgotten)  // Denne blir undefined...
+        } catch (err) {
+            this.errorMsg = err.message;
+        }
+    }
+
     cancel() {
         this.$emit("cancel");
     }
