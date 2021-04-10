66bff2968430060b95269335089852212dfbee48
h577499 <toril.sunde.apelthun@gmail.com>
2021-04-10 19:24:29 +0200

    Added a AdminNavBar and some admin components

---
+++ clients/webclient/src/components/Admin/Admin.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-app>
+		<admin-nav-bar></admin-nav-bar>
+	</v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import AdminNavBar from './AdminNavBar.vue'
+
+@Component({
+	components: {
+        AdminNavBar, 
+    },
+})
+export default class App extends Vue {}
+</script>
