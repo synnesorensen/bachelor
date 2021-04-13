ff597a1d843b540311dacee8dd251f4c4e873aaf
h577499 <toril.sunde.apelthun@gmail.com>
2021-04-13 15:37:58 +0200

    Changed calendar to only show weekdays

--- clients/webclient/src/components/Admin/AdminOverview.vue
+++ clients/webclient/src/components/Admin/AdminOverview.vue
@@ -1,18 +1,18 @@
 <template>
-	<calendar-overview></calendar-overview>
+	<v-main>
+		<calendar-overview></calendar-overview>
+	</v-main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import CalendarOverview from '../Calendar/CalendarOverview.vue'
+import CalendarOverview from '../Calendar/CalendarOverview.vue';
 
 @Component({
-    components: {
+	components: {
 		CalendarOverview,
-    },
+	},
 })
-export default class AdminOverview extends Vue {
-
-}
+export default class AdminOverview extends Vue {}
 </script>
--- clients/webclient/src/components/Calendar/CalendarOverview.vue
+++ clients/webclient/src/components/Calendar/CalendarOverview.vue
@@ -1,6 +1,7 @@
 <template>
-	<v-sheet height="600">
-		<v-calendar locale="no" color="primary"> </v-calendar>
+	<v-sheet height="600" width="70%">
+		<h2>Her skal det være tekst</h2>
+		<v-calendar locale="no" color="primary" weekdays="1, 2, 3, 4, 5"> </v-calendar>
 	</v-sheet>
 </template>
 
@@ -9,7 +10,14 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 
 @Component
-export default class Calendar extends Vue {
-    private tab = 0;
+export default class CalendarOverview extends Vue {
+	// data: () => {
+	// 	type: 'month';
+	// 	start: '2019-01-01';
+	// 	end: '2021-12-01';
+	// 	typeOptions: [{ text: 'Dag'; value: 'day' }];
+	// 	shortWeekdays: false;
+	// 	weekdays: [1, 2, 3, 4, 5];
+	// };
 }
 </script>
