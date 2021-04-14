a6d809264db5e4aaf1321c79037437653ea06609
Synnøve Sørensen <578010@stud.hvl.no>
2021-04-14 22:29:50 +0200

    Added member variables and started on allergy list

--- clients/webclient/src/components/CustomerOrder.vue
+++ clients/webclient/src/components/CustomerOrder.vue
@@ -8,30 +8,30 @@
 			</v-row>
 			<v-row class="text-center">
 				<v-col>
-					<v-text-field label ="Fornavn" v-model="Fornavn"></v-text-field>
+					<v-text-field label ="Fornavn" v-model="firstName"></v-text-field>
 				</v-col>
             	<v-col>
-                	<v-text-field label ="Etternavn" v-model="Etternavn"></v-text-field>
+                	<v-text-field label ="Etternavn" v-model="lastName"></v-text-field>
 				</v-col>
 			</v-row>
         	<v-row class ="text-center">
             	<v-col>
-                	<v-text-field label="Gateadresse" v-model="Gateadresse"></v-text-field>
+                	<v-text-field label="Gateadresse" v-model="address"></v-text-field>
             	</v-col>
         	</v-row>
         	<v-row class ="text-center">
             	<v-col cols="4">
-                	<v-text-field label="Postnummer" v-model="Postnummer"></v-text-field>
+                	<v-text-field :rules="[numbers, length]" label="Postnummer" v-model="postNo"></v-text-field>
             	</v-col>
             	<v-col>
-                	<v-text-field label="Poststed" v-model="Poststed"></v-text-field>
+                	<v-text-field label="Poststed" v-model="postPlace"></v-text-field>
             	</v-col>
         	</v-row>
         	<v-row>
             	<h4>Antall porsjoner</h4>
         	</v-row>
         	<v-row>
-            	<v-chip-group v-model="days" active-class="blue--text text--accent-4" mandatory>
+            	<v-chip-group active-class="blue--text text--accent-4" mandatory>
                 	<v-chip filter outlined>
                     	1
                 	</v-chip>
@@ -63,12 +63,12 @@
                     	10
                 	</v-chip>
             	</v-chip-group>
-        	</v-row>
+        	</v-row>      
         	<v-row>
             	<h4>Velg ønsket levering</h4>
         	</v-row>
         	<v-row>
-            	<v-chip-group v-model="days" active-class="blue--text text--accent-4" multiple>
+            	<v-chip-group active-class="blue--text text--accent-4" multiple>
                 	<v-chip filter outlined>
                     	Lunsj tirsdag
                 	</v-chip>
@@ -87,15 +87,12 @@
             	<h4>Hvor mange leveringer ønsker du?</h4>
         	</v-row>
         	<v-row>
-            	<v-chip-group v-model="days" active-class="blue--text text--accent-4" mandatory>
+            	<v-chip-group active-class="blue--text text--accent-4" mandatory>
                 	<v-chip filter outlined>
                     	Enkel levering (kr. 149)
                 	</v-chip>
                 	<v-chip filter outlined>
-                    	Fire uker (kr. 137 per levering)
-                	</v-chip>
-                	<v-chip filter outlined>
-                    	Åtte uker (kr. 137 per levering)
+                    	Abonnement (kr. 137 per levering)
                 	</v-chip>
             	</v-chip-group>
         	</v-row>
@@ -103,7 +100,7 @@
             	<h4>Hvilken type bokser ønsker du?</h4>
         	</v-row>
         	<v-row>
-            	<v-chip-group v-model="days" active-class="blue--text text--accent-4" mandatory>
+            	<v-chip-group active-class="blue--text text--accent-4" mandatory>
                 	<v-chip filter outlined>
                     	Engangsbokser
                 	</v-chip>
@@ -116,38 +113,21 @@
             	<h4>Har du noen allergier</h4>
         	</v-row>
         	<v-row>
-            	<v-chip-group v-model="days" active-class="blue--text text--accent-4" multiple>
-                	<v-chip filter outlined>
-                    	Nøtter
-                	</v-chip>
-                	<v-chip filter outlined>
-                    	Sjømat
-                	</v-chip>
-                	<v-chip filter outlined>
-                    	Gluten
-                	</v-chip>
-                	<v-chip filter outlined>
-                    	Laktose
-                	</v-chip>
-                	<v-chip filter outlined>
-                    	Melkeprotein
-                	</v-chip>
-                	<v-chip filter outlined>
-                    	Hvete
-                	</v-chip>
-                	<v-chip filter outlined>
-                    	Løk
-                	</v-chip>
-                	<v-chip filter outlined>
-                    	Tomat
+            	<v-chip-group active-class="blue--text text--accent-4" multiple>
+                	<v-chip 
+                        v-for="allergy in allergies" 
+                        v-bind:key="allergy.name" 
+                        v-model="allergy.selected"
+                        filter outlined>
+                    {{allergy.name}}
                 	</v-chip>
             	</v-chip-group>
         	</v-row>
         	<v-row>
-            	<v-text-field label="Annen informasjon du vil legge til?" v-model="Annet"></v-text-field>
+            	<v-text-field label="Annen informasjon du vil legge til?" v-model="add"></v-text-field>
         	</v-row>
         	<v-row>
-            	<v-btn>Send</v-btn>
+            	<v-btn @click="sendToDb">Send</v-btn>
         	</v-row>
 		</v-container>
     </v-form>
@@ -159,6 +139,37 @@ import Component from 'vue-class-component';
 
 @Component
 export default class CustomerOrder extends Vue {
+    private firstName = "";
+    private lastName = "";
+    private address = "";
+    private postNo = 0;
+    private postPlace = "";
+    private noOfMeals = 1;
+    private deliveryDay = "";
+    private deliveryType = "";
+    private boxType = "";
+    private allergies = [
+        {
+            name: "Gluten",
+            selected: false
+        },
+        {
+            name: "Melk",
+            selected: false
+        } 
+    ];
+    private add = "";
+
+    // Rules:
+    numbers(value: string) {
+        return !isNaN(parseInt(value)) || "Vennligst oppgi et gyldig postnummer";
+    }
+    length(value: string) {
+        return value.length == 4 || "Vennligst oppgi et gyldig postnummer";
+    }
     
+    sendToDb() {
+
+    }
 }
 </script>
