c20a2c6ef6dc14c2024d1f2b696cf92fb1c2b08f
Synnøve Sørensen <578010@stud.hvl.no>
2021-04-18 22:37:50 +0200

    Refreshing jwt

--- clients/webclient/src/api/api.ts
+++ clients/webclient/src/api/api.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import {urlPrefix} from '../../../../common/settings'
 import * as interfaces from '../../../../server/src/interfaces'
+import getAuth from '../components/LoginDialog/auth'
 
 export let apiAxios = axios.create();
+let jwtLastRefreshed = 0;
+
 export function setApiBearerToken(token: string) {
+    jwtLastRefreshed = Date.now();
     apiAxios = axios.create({
         headers: {
             common: {
@@ -13,11 +17,22 @@ export function setApiBearerToken(token: string) {
     });
 }
 
+async function ensureFreshToken() {
+    if (jwtLastRefreshed < (Date.now() - (1000*60))) {
+        let Auth = getAuth();
+        let jwtToken = (await Auth.currentSession()).getIdToken().getJwtToken();
+        console.log("It is old:", jwtToken);
+        setApiBearerToken(jwtToken);
+    }
+}
+
 export async function getVendorSubscriptions():Promise<interfaces.UserSubscription[]> {
+    await ensureFreshToken();
     return await apiAxios.get(urlPrefix + "/v/subscriptions");
 }
 
 export async function getUserprofile():Promise<interfaces.Userprofile | null> {
+    await ensureFreshToken();
     try {
         const userprofile = await apiAxios.get(urlPrefix + "/userprofile");
         return userprofile.data;
@@ -30,11 +45,13 @@ export async function getUserprofile():Promise<interfaces.Userprofile | null> {
 }
 
 export async function putUserprofile(userprofile: interfaces.Userprofile):Promise<interfaces.Userprofile> {
+    await ensureFreshToken();
     const addedUser = await apiAxios.put(urlPrefix + "/userprofile", userprofile);
     return addedUser.data;
 }
 
 export async function putSubscription(subscription: interfaces.Subscription):Promise<interfaces.Subscription> {
+    await ensureFreshToken();
     const addedSubscription = await apiAxios.put(urlPrefix + "/u/subscription?vendorId=" + subscription.vendorId, subscription);
     return addedSubscription.data;
 }
