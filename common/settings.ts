61c788da17d477bef6ebac69ac522e6901ba6491
h577499 <toril.sunde.apelthun@gmail.com>
2021-04-22 23:02:01 +0200

    Fikset tabell i settings

--- common/settings.ts
+++ common/settings.ts
@@ -1,6 +1,6 @@
 export const urlPrefix = "http://localhost:3000/dev";
 
-export const TABLENAME = "GlobalTable";
+export const TABLENAME = process.env.TABLENAME;
 export const REGION = "eu-north-1";
 
 export const awsCognitoUserPoolId = process.env.VUE_APP_AWS_COGNITO_USER_POOL_ID;
