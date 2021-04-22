089160cfc0bbaac01fa3c09d5deaddbb3af05fca
Synnøve Sørensen <578010@stud.hvl.no>
2021-04-22 09:26:09 +0200

    Added environmental variables for aws id

--- common/settings.ts
+++ common/settings.ts
@@ -1,7 +1,7 @@
-export const urlPrefix = "https://4zhpwcxtq4.execute-api.eu-north-1.amazonaws.com/dev";
+export const urlPrefix = "https://localhost:3000/dev";
 
 export const TABLENAME = "GlobalTable";
 export const REGION = "eu-north-1";
 
-export const awsCognitoUserPoolId = "eu-north-1_T0CZQQ1dX";
-export const awsCognitoAppClientId = "4c4g5j647lbdm1kn07i54nl425";
+export const awsCognitoUserPoolId = process.env.AWS_COGNITO_USER_POOL_ID
+export const awsCognitoAppClientId = process.env.AWS_COGNITO_APP_CLIENT_ID
