15d9c3865407a032e8a96e273b87b545d3ec3e0a
ihjelle <ihjelle@me.com>
2021-04-23 11:10:32 +0200

    Added functionality to see userprofile

--- clients/webclient/src/components/CustomerProfile.vue
+++ clients/webclient/src/components/CustomerProfile.vue
@@ -1,20 +1,141 @@
 <template>
   <v-container>
-      <v-row class="text-center">
-        <v-col cols="12">
-            <h3>Dette er CustomerProfile-komponenten</h3>
-        </v-col>
-      </v-row>
+    <v-row>
+      <v-col>
+        <h1 class="primary--text">Min profil</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h2>{{ fullname }}</h2>
+      </v-col>
+      <v-col>
+        <v-btn class="mx-2" fab dark small color="primary">
+          <v-icon dark> mdi-pencil </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h3>{{ address }}</h3>
+      </v-col>
+      <v-col>
+        <v-btn class="mx-2" fab dark small color="primary">
+          <v-icon dark> mdi-pencil </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h3>{{ phone }}</h3>
+      </v-col>
+      <v-col>
+        <v-btn class="mx-2" fab dark small color="primary">
+          <v-icon dark> mdi-pencil </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h3>{{ email }}</h3>
+      </v-col>
+      <v-col>
+        <v-btn class="mx-2" fab dark small color="primary">
+          <v-icon dark> mdi-pencil </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h3>Mine allergier: {{ allergies }}</h3>
+      </v-col>
+      <v-col>
+        <v-btn class="mx-2" fab dark small color="primary">
+          <v-icon dark> mdi-pencil </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h3>Antall porsjoner: {{ noOfMeals }}</h3>
+      </v-col>
+      <v-col>
+        <v-btn class="mx-2" fab dark small color="primary">
+          <v-icon dark> mdi-pencil </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h3>Valgt boks: {{ box }}</h3>
+      </v-col>
+      <v-col>
+        <v-btn class="mx-2" fab dark small color="primary">
+          <v-icon dark> mdi-pencil </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h3>Leveringsdag: {{ schedule }}</h3>
+      </v-col>
+      <v-col>
+        <v-btn class="mx-2" fab dark small color="primary">
+          <v-icon dark> mdi-pencil </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
+import { getUserprofile, getUserSubscription } from "../api/api";
 
 @Component
 export default class CustomerProfile extends Vue {
+  //Userprofile
+  private fullname = "";
+  private address = "";
+  private phone = "";
+  private email = "";
+  private allergies = [];
 
+  //Usersubscription
+  private approved = false;
+  private paused = false;
+  private schedule = [];
+  private noOfMeals = "";
+  private box  =  "";
+
+
+async showUserProfile() {
+    let userRes = await getUserprofile();
+    if(userRes != null) {
+        this.fullname = userRes.fullname;
+        this.address = userRes.address;
+        this.phone = userRes.phone;
+        this.email = userRes.email;
+        this.allergies = userRes.allergies;
+    }
+
+    //Må finnne en bedre måte å hente vendor på
+    let vendor = "lunsj@hjul.no";
+    let subscriptionRes = await getUserSubscription(vendor);
+    if(subscriptionRes != null) {
+      this.approved = subscriptionRes.approved;
+      this.paused = subscriptionRes.paused;
+      this.noOfMeals = subscriptionRes.noOfMeals.toString();
+      this.box = subscriptionRes.box;
+      this.schedule = subscriptionRes.schedule;
+      console.log(subscriptionRes)
+    }
+
+    }
+
+  beforeMount() {
+      this.showUserProfile();
+  }
 }
-
 </script>
