f1c74f08e483ad332341809b7325b565750e8e76
Synnøve Sørensen <578010@stud.hvl.no>
2021-04-26 21:17:46 +0200

    Added most http-methods

--- clients/webclient/src/api/api.ts
+++ clients/webclient/src/api/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { url } from 'node:inspector';
 import {urlPrefix} from '../../../../common/settings'
 import * as interfaces from '../../../../server/src/interfaces'
 import getAuth from '../components/LoginDialog/auth'
@@ -25,29 +26,11 @@ async function ensureFreshToken() {
     }
 }
 
-export async function getVendorSubscriptions():Promise<interfaces.UserSubscription[]> {
-    await ensureFreshToken();
-    return await apiAxios.get(urlPrefix + "/v/subscriptions");
-}
-
 export async function getUserprofile():Promise<interfaces.Userprofile | null> {
     await ensureFreshToken();
     try {
         const userprofile = await apiAxios.get(urlPrefix + "/userprofile");
         return userprofile.data;
-    } catch (error) {
-        if (error.response.status == 404) {
-            return null;
-        }
-        throw (error);
-    }
-}
-
-export async function getUserSubscription(id: string):Promise<interfaces.UserSubscription | null> {
-    await ensureFreshToken();
-    try {
-        const userSubscription = await apiAxios.get(urlPrefix + "/u/subscription?vendorId=" + id);
-        return userSubscription.data;
     } catch (error) {
         if (error.response.status == 404) {
             return null;
@@ -62,8 +45,112 @@ export async function putUserprofile(userprofile: interfaces.Userprofile):Promis
     return addedUser.data;
 }
 
-export async function putSubscription(subscription: interfaces.Subscription):Promise<interfaces.Subscription> {
+export async function deleteUserprofile() {
+    await ensureFreshToken();
+    await apiAxios.delete(urlPrefix + "/userprofile");
+}
+
+export async function getVendor(vendorId: string): Promise<interfaces.Vendor | null> {
+    await ensureFreshToken();
+    try {
+        const vendor = await apiAxios.get(urlPrefix + "/vendor?vendorId=" + vendorId);
+        return vendor.data;
+    } catch (error) {
+        if (error.response.status == 404) {
+            return null;
+        }
+        throw (error);
+    }
+}
+
+export async function putVendor(vendor: interfaces.Vendor): Promise<interfaces.Vendor> {
+    await ensureFreshToken();
+    const addedVendor = await apiAxios.put(urlPrefix + "/vendor", vendor);
+    return addedVendor.data;
+}
+
+export async function deleteVendor() {
+    await ensureFreshToken();
+    await apiAxios.delete(urlPrefix + "/vendor");
+}
+
+// TODO: Lag en funksjon for å hente liste av vendors når tilhørende lambda er implementert.
+
+export async function getVendorSubscriptions():Promise<interfaces.UserSubscription[]> {
+    await ensureFreshToken();
+    return await apiAxios.get(urlPrefix + "/v/subscriptions");
+}
+
+export async function getUserSubscriptions():Promise<interfaces.CompanySubscription[]> {
+    await ensureFreshToken();
+    return await apiAxios.get(urlPrefix + "/u/subscriptions");
+}
+
+export async function getVendorSubscription(id: string):Promise<interfaces.Subscription | null> {
+    await ensureFreshToken();
+    try {
+        const vendorSubscription = await apiAxios.get(urlPrefix + "/v/subscription?userId=" + id);
+        return vendorSubscription.data;
+    } catch (error) {
+        if (error.response.status == 404) {
+            return null;
+        }
+        throw (error);
+    }
+}
+
+export async function putVendorSubscription(subscription: interfaces.Subscription):Promise<interfaces.Subscription> {
+    await ensureFreshToken();
+    const addedSubscription = await apiAxios.put(urlPrefix + "/v/subscription?userId=" + subscription.userId, subscription);
+    return addedSubscription.data;
+}
+
+export async function deleteVendorSubscription(id: string) {
+    await ensureFreshToken();
+    await apiAxios.delete(urlPrefix + "/v/subscription?userId=" + id);
+}
+
+export async function getUserSubscription(id: string):Promise<interfaces.Subscription | null> {
+    await ensureFreshToken();
+    try {
+        const userSubscription = await apiAxios.get(urlPrefix + "/u/subscription?vendorId=" + id);
+        return userSubscription.data;
+    } catch (error) {
+        if (error.response.status == 404) {
+            return null;
+        }
+        throw (error);
+    }
+}
+
+export async function putUserSubscription(subscription: interfaces.Subscription):Promise<interfaces.Subscription> {
     await ensureFreshToken();
     const addedSubscription = await apiAxios.put(urlPrefix + "/u/subscription?vendorId=" + subscription.vendorId, subscription);
     return addedSubscription.data;
 }
+
+export async function deleteUserSubscription(id: string) {
+    await ensureFreshToken();
+    await apiAxios.delete(urlPrefix + "/u/subscription?vendorId=" + id);
+}
+
+export async function getAllVendorsDeliveries(start: string, end: string):Promise<interfaces.Delivery[] | null> {
+    await ensureFreshToken();
+    try {
+        const deliveries = await apiAxios.get(urlPrefix + "/v/deliveries?start=" + start + "&end=" + end);
+        return deliveries.data;
+    } catch (error) {
+        if (error.response.status == 404) {
+            return null;
+        }
+        throw (error);
+    }
+}
+
+export async function addNewDeliveries(deliveries: interfaces.Delivery[]): Promise<interfaces.Delivery[]> {
+    await ensureFreshToken();
+    const addedDeliveries = await apiAxios.post(urlPrefix + "/v/deliveries", deliveries);
+    return addedDeliveries.data;
+}
+
+
