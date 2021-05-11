2c531b9dffbde15068efdda5ad9a08851070ed85
ihjelle <ihjelle@me.com>
2021-05-11 13:00:50 +0200

    saving

---
+++ clients/webclient/src/components/Users/CustomerDeliveries.vue
@@ -0,0 +1,41 @@
+ <template>
+         <v-row>
+        <template>
+            <v-dialog dialog="true" v-model="dialog" persistent max-width="290"> 
+                <v-card>
+                    <v-card-title class="headline">
+                        Hå
+                    </v-card-title> 
+                    <v-card-text
+                        > Hei
+                    </v-card-text>
+                    <v-btn
+                        color="green darken-1"
+                        text
+                        @click="dialog = false">
+                        OK!
+                    </v-btn>
+                    <v-btn
+                        color="green darken-1"
+                        text
+                        @click="dialog = false">
+                        Avbryt
+                    </v-btn>
+                </v-card>
+                </v-dialog>
+            </template>
+        </v-row>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import  api from "../../api/api";
+
+@Component
+export default class CustomerDeliveries extends Vue {
+    private dialog = false;
+
+}
+</script>
--- clients/webclient/src/components/Users/CustomerOverview.vue
+++ clients/webclient/src/components/Users/CustomerOverview.vue
@@ -1,104 +1,153 @@
 <template>
-	<v-row class="fill-height">
-		<v-col>
-			<v-sheet height="64">
-				<v-spacer></v-spacer>
-				<v-toolbar flat>
-					<v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
-						I dag
-					</v-btn>
-					<v-btn fab text stmall color="grey darken-2" @click="prev">
-						<v-icon small>mdi-chevron-left</v-icon>
-					</v-btn>
-					<v-btn fab text stmall color="grey darken-2" @click="next">
-						<v-icon small>mdi-chevron-right</v-icon>
-					</v-btn>
-					<v-toolbar-title v-if="$refs.calendar">
-						{{ $refs.calendar.title }}
-					</v-toolbar-title>
-				</v-toolbar>
-			</v-sheet>
-			<v-spacer></v-spacer>
-			<v-sheet height="600">
-				<v-calendar
-					ref="calendar"
-					v-model="focus"
-					locale="no"
-					color="primary"
-					weekdays="1, 2, 3, 4, 5"
-                    :events="events"
-					:now="today"
-                    show-week
-                    @click:event="showEvent"
-                    @change="getEvents"
-				>
-				</v-calendar>
-			</v-sheet>
-		</v-col>
-	</v-row>
+  <v-row class="fill-height">
+    <v-col>
+      <v-sheet height="64">
+        <v-spacer></v-spacer>
+        <v-toolbar flat>
+          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+            I dag
+          </v-btn>
+          <v-btn fab text stmall color="grey darken-2" @click="prev">
+            <v-icon small>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-btn fab text stmall color="grey darken-2" @click="next">
+            <v-icon small>mdi-chevron-right</v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">
+            {{ $refs.calendar.title }}
+          </v-toolbar-title>
+        </v-toolbar>
+      </v-sheet>
+      <v-spacer></v-spacer>
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          v-model="focus"
+          locale="no"
+          color="primary"
+          weekdays="1, 2, 3, 4, 5"
+          :events="events"
+          :now="today"
+          show-week
+          @click:event="showEvent"
+          @change="getEvents"
+        >
+        </v-calendar>
+      </v-sheet>
+    </v-col>
+    <v-col>
+      <template>
+        <v-dialog
+          v-if="show"
+          :date="selectedDate"
+          dialog="true"
+          v-model="dialog"
+          persistent
+          max-width="400"
+        >
+          <v-card>
+            <v-card-title class="headline"> Kansellere levering {{this.selectedDate}} </v-card-title>
+            <v-card-text> Er du sikker på at du ønsker å kansellere leveering {{this.selectedDate}} </v-card-text>
+            <v-btn color="green darken-1" text @click="dialog = false">
+              OK!
+            </v-btn>
+            <v-btn color="green darken-1" text @click="dialog = false">
+              Avbryt
+            </v-btn>
+          </v-card>
+        </v-dialog>
+      </template>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import  api from "../../api/api";
+import Vue from "vue";
+import Component from "vue-class-component";
+import api from "../../api/api";
+import CustomerDeliveries from "./CustomerDeliveries.vue";
 
-@Component
+@Component({
+  components: {
+    CustomerDeliveries,
+  },
+})
 export default class CustomerOverview extends Vue {
-	private today = new Date().toISOString().substr(0, 10);
-	private focus = new Date().toISOString().substr(0, 10);
-	private type = "month";
-	private start = null;
-	private end = null;
-    private events: any[] =[];
-    private selectedEvent = "";
+  private today = new Date().toISOString().substr(0, 10);
+  private focus = new Date().toISOString().substr(0, 10);
+  private type = "month";
+  private start = null;
+  private end = null;
+  private events: any[] = [];
+  private selectedEvent = "";
+  private show: boolean = false;
+  private selectedDate = "";
+  private dialog: boolean = false;
 
-	mounted() {
-		this.focus = "";
-	}
-	viewDay(day: any) {
-		this.focus = day.date;
-		this.type = "day";
-	}
-	setToday() {
-		this.focus = "";
-	}
-	prev() {
-		(this.$refs.calendar as any).prev();
-	}
-	next() {
-		(this.$refs.calendar as any).next();
-	}
-	updateRange(range: any) {
-		this.start = range.start;
-		this.end = range.end;
-	}
+  mounted() {
+    this.focus = "";
+  }
+  viewDay(day: any) {
+    this.focus = day.date;
+    this.type = "day";
+  }
+  setToday() {
+    this.focus = "";
+  }
+  prev() {
+    (this.$refs.calendar as any).prev();
+  }
+  next() {
+    (this.$refs.calendar as any).next();
+  }
+  updateRange(range: any) {
+    this.start = range.start;
+    this.end = range.end;
+  }
 
-    async getEvents( {start, end}:{start:any, end:any} ) {
-        let events: any[] = [];
-        let deliveries = await api.getAllUsersDeliveries(start.date, end.date);
-        let sub = await api.getUserSubscriptions();
-        let schedule = sub[0].schedule;
+  async getEvents({ start, end }: { start: any; end: any }) {
+    let events: any[] = [];
+    let deliveries = await api.getAllUsersDeliveries(start.date, end.date);
+    let sub = await api.getUserSubscriptions();
+    let schedule = sub[0].schedule;
+	let color = "red";
 
-        if (deliveries) {
-            deliveries.forEach((del) => {
-                const delStart = new Date(`${del.deliverytime.substring(0,10)}T00:00:00`);
-                const delEnd = new Date(`${del.deliverytime.substring(0,10)}T23:59:59`);
-                const menu = schedule.find(({id}) => id == del.menuId);
+    if (deliveries) {
+      deliveries.forEach((del) => {
+        const delStart = new Date(
+          `${del.deliverytime.substring(0, 10)}T00:00:00`
+        );
+        const delEnd = new Date(
+          `${del.deliverytime.substring(0, 10)}T23:59:59`
+        );
+        const menu = schedule.find(({ id }) => id == del.menuId);
+		if(del.cancelled) {
+			color
+		}
 
-                events.push({
-                    name: menu!.menu,
-                    start: delStart,
-                    end: delEnd,
-                    color: "green"
-                });
-            });
-            this.events = events;
-        }
+        events.push({
+          name: menu!.menu,
+          start: delStart,
+          end: delEnd,
+          color: "green",
+        });
+      });
+      this.events = events;
     }
-    showEvent(nativeEvent:any, event:any) {
-        this.selectedEvent = event;
-        
-    }
+  }
+  showEvent(event: any) {
+    this.show = true;
+    this.dialog = true;
+    this.selectedDate = event.day.date;
+    this.selectedEvent = event;
+  }
+
+  async changeDelivery() {
+	  let user = await api.getUserprofile();
+	  let vend = await api.getVendorSubscriptions();
+	  let res = await api.getDelivery(vend[0].vendorId, user.email, this.selectedDate);
+	  res.cancelled = true;
+	  await api.putDelivery(vend[0].vendorId, user.email, res);
+  }
 }
 </script>
