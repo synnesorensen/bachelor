71e1d2141172fb1a0f3cfc1c0f3d9ccd08786db6
ihjelle <ihjelle@me.com>
2021-05-20 13:15:52 +0200

    made form simpler

--- clients/webclient/src/components/Users/CustomerEdit.vue
+++ clients/webclient/src/components/Users/CustomerEdit.vue
@@ -9,38 +9,16 @@
             <v-row class="text-center">
                 <v-col>
                     <v-text-field
-                        label="Fornavn"
-                        v-model="firstName"
-                    ></v-text-field>
-                </v-col>
-                <v-col>
-                    <v-text-field
-                        label="Etternavn"
-                        v-model="lastName"
+                        label="Fullt navn"
+                        v-model="fullname"
                     ></v-text-field>
                 </v-col>
             </v-row>
             <v-row class="text-center">
                 <v-col>
                     <v-text-field
-                        label="Gateadresse"
+                        label="Adresse"
                         v-model="address"
-                    ></v-text-field>
-                </v-col>
-            </v-row>
-            <v-row class="text-center">
-                <v-col cols="4">
-                    <v-text-field
-                        :rules="[numbers, postNoLength]"
-                        label="Postnummer"
-                        v-model="postNo"
-                        required
-                    ></v-text-field>
-                </v-col>
-                <v-col>
-                    <v-text-field
-                        label="Poststed"
-                        v-model="postPlace"
                     ></v-text-field>
                 </v-col>
             </v-row>
@@ -65,11 +43,11 @@
                 <h4>Dine registrerte alleriger er</h4>
             </v-row>
             <v-row>
-                {{ this.selectedAllergies }}
+                {{ this.allergies }}
             </v-row>
             <v-row>
                 <v-col>
-                    <v-btn @click="sendToDb" color="primary" class="ma-1"
+                    <v-btn @click="sendToDb(component)" color="primary" class="ma-1"
                         >Send inn</v-btn
                     >
                 </v-col>
@@ -84,89 +62,66 @@ import Component from "vue-class-component";
 import { Prop } from "vue-property-decorator";
 import api from "../../api/api";
 import { Userprofile } from "../../../../../server/src/interfaces";
+import CustomerProfile from "../Users/CustomerProfile.vue";
 
-@Component
+@Component({
+    components: {
+        CustomerProfile,
+    },
+})
 export default class CustomerEdit extends Vue {
     @Prop() loggedInUser!: string;
     @Prop() userprofile!: Userprofile;
-    private firstName = "";
-    private lastName = "";
+    private fullname = "";
     private address = "";
-    private postNo = "";
-    private postPlace = "";
     private phone = "";
     private email = "";
+    private allergies: string[] = [];
     private show = true;
+    private editUserprofile: boolean = true;
+    private showUserprofile: boolean = false;
+    private component = CustomerEdit;
 
     async fillForm() {
         let user = await api.getUserprofile();
-        let fn = user.fullname.split(" ");
-        for (let i = 0; i < fn.length - 1; i++) {
-            this.firstName += fn[i] + " ";
+        if (user) {
+            this.fullname = user.fullname;
+            this.address = user.address;
+            this.phone = user.phone;
+            this.email = user.email;
+            this.allergies = user.allergies;
+            console.log(this.userprofile);
         }
-        this.lastName = fn[fn.length - 1];
-        let a = user.address.split(" ");
-        this.address = a[0] + " " + a[1];
-        this.postNo = a[2];
-        this.postPlace = a[3];
-        this.phone = user.phone;
-        this.email = user.email;
-        this.selectedAllergies = user.allergies;
-        console.log(this.userprofile);
     }
 
     async beforeMount() {
         this.fillForm();
     }
 
-    private allergies = [
-        { name: "Gluten", selected: false },
-        { name: "Skalldyr", selected: false },
-        { name: "Egg", selected: false },
-        { name: "Fisk", selected: false },
-        { name: "Peanøtter", selected: false },
-        { name: "Nøtter", selected: false },
-        { name: "Melk", selected: false },
-        { name: "Soya", selected: false },
-        { name: "Selleri", selected: false },
-        { name: "Sennep", selected: false },
-        { name: "Sesam", selected: false },
-        { name: "Svovel", selected: false },
-        { name: "Lupin", selected: false },
-        { name: "Bløtdyr", selected: false },
-    ];
-    private selectedAllergies = [];
-
-    // Rules:
     numbers(value: string) {
         return (
             !isNaN(parseInt(value)) || "Vennligst oppgi et gyldig postnummer"
         );
     }
-    postNoLength(value: string) {
-        return value.length == 4 || "Vennligst oppgi et gyldig postnummer";
-    }
+
     phoneNoLength(value: string) {
         return value.length >= 8 || "Vennligst oppgi et gyldig telefonnummer";
     }
 
-    async sendToDb() {
-        this.show = false;
-        let newUserprofile = {
-            fullname: this.firstName + " " + this.lastName,
-            address: this.address + " " + this.postNo + " " + this.postPlace,
+    async sendToDb(component) {
+        let updateUserprofile = {
+            fullname: this.fullname,
+            address: this.address,
             phone: this.phone.toString(),
             email: this.email,
-            allergies: this.selectedAllergies,
+            allergies: this.allergies,
             isVendor: false,
         };
 
-        await api.putUserprofile(newUserprofile);
-        this.$emit("newUserprofile", newUserprofile);
-    }
-
-    cancel() {
-       this.show = false;
+        await api.putUserprofile(updateUserprofile);
+        this.editUserprofile = false;
+        this.showUserprofile = true;
+        component = CustomerProfile;
     }
 }
 </script>
