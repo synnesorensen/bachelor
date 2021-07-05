a706ff001b3594b7927c087ad08956876032dae9
Synnøve Sørensen <578010@stud.hvl.no>
2021-07-05 18:32:51 +0200

    Prettified welcome page

--- clients/webclient/src/views/Welcome.vue
+++ clients/webclient/src/views/Welcome.vue
@@ -1,13 +1,43 @@
 <template>
-    <v-container>
-        <h1>Velkommen til oss</h1>
-        <p>Er du eksisterende kunde, trykk på logg inn-knappen. 
-            Ønsker du å bli kunde, trykk på register-knappen.
-        </p>
-        <v-btn @click="showLogInBox = true">Logg inn</v-btn>
-        <v-btn @click="showRegisterBox = true">Registrer meg</v-btn>
-        <LoginDialog @loggedIn="loggedIn" :showDialog="showLogInBox" />
-        <RegisterDialog @loggedIn="loggedIn" :showDialog="showRegisterBox" />
+    <v-container fluid>
+        <v-row dense>
+        <v-col :xl="6" :lg="8">
+            <v-card>
+                <v-img
+                    src="..\..\assets\smorblomst_crop1.jpg"
+                    gradient="to top right, rgba(80,115,80,1), rgba(25,32,25,0)"
+                    required
+                    class="white--text align-end"
+                    height="40%"
+                >
+                    <v-card-title>Velkommen til Lunsj på hjul</v-card-title>
+                </v-img>
+                <v-card-text>Her kan kunder som allerede er registrerte logge seg inn. <br /> 
+                Nye kunder kan registrere seg ved å trykke på knappen Registrer. 
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn 
+                        color="orange"
+                        text
+                        @click="showLogInBox = true" 
+                        class="mx-2"
+                    >
+                        Logg inn
+                    </v-btn>
+                    <v-btn 
+                        color="orange"
+                        text
+                        @click="showRegisterBox = true" 
+                        class="mx-2"
+                    >
+                        Registrer
+                    </v-btn>
+                    <LoginDialog @closeDialog="closeDialog" @loggedIn="loggedIn" :showDialog="showLogInBox" />
+                    <RegisterDialog @closeDialog="closeDialog" @loggedIn="loggedIn" :showDialog="showRegisterBox" />
+                </v-card-actions>
+            </v-card>
+        </v-col>
+        </v-row>
     </v-container>
 </template>
 
@@ -36,6 +66,11 @@ export default class Welcome extends Vue{
                 self.showLogInBox = false;
                 self.showRegisterBox = false;
         }});
+    }
+
+    closeDialog() {
+        this.showLogInBox = false;
+        this.showRegisterBox = false;
     }
 };
 </script>
