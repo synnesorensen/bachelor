3cdfd724b44feec67f3de7630ed41f754a743b7e
Synnøve Sørensen <578010@stud.hvl.no>
2022-07-10 12:27:57 +0200

    WIP: adjusted top card, added pointer to bottom cards

--- clients/webclient/src/views/Welcome.vue
+++ clients/webclient/src/views/Welcome.vue
@@ -1,42 +1,50 @@
 <template>
-  <v-container >
+  <v-container>
     <v-row justify="center" dense>
       <v-col :xl="8" :lg="8" :md="10">
         <v-row>
-          <v-img
-            src="..\..\assets\BA.jpg"
-            required
-            height="40%"
-            gradient="to bottom right, rgba(10,10,10,0.3),rgba(0,0,0,0)"
-          >
-            <v-col>
-              <v-row
-                style="
+          <v-col cols="12">
+            <v-card>
+              <v-img
+                src="..\..\assets\BA.jpg"
+                required
+                gradient="to bottom right, rgba(10,10,10,0.3),rgba(0,0,0,0)"
+              >
+                <v-col>
+                  <v-row
+                    style="
                     background-color: rgba(30,30,30,0.1)
                     border-radius: 2px"
-              >
-                <v-col :xl="8" :lg="8" :md="10">
-                  <v-row class="ml-4">
-                    <p class="mt-8 text-h4 white--text">Velkommen til Lunsj på Hjuls kundesider</p>
+                  >
+                    <v-col :xl="8" :lg="8" :md="10">
+                      <v-row class="ml-4">
+                        <p class="mt-8 text-h4 white--text">
+                          Velkommen til Lunsj på Hjuls kundesider
+                        </p>
+                      </v-row>
+                    </v-col>
+                    <v-col>
+                      <v-row class="justify-end mt-4 mr-4"> </v-row>
+                    </v-col>
                   </v-row>
                 </v-col>
-                <v-col >
-                  <v-row class="justify-end mt-4 mr-4">
-                  </v-row>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-img>
+              </v-img>
+            </v-card>
+          </v-col>
         </v-row>
-        <v-row class="mt-8 px-48">
+        <v-row class="my-4">
           <v-col cols="4">
             <v-card height="350">
               <v-img
                 src="..\..\assets\Kort1.jpg"
                 class="white--text align-end"
                 height="280px"
+                @click="showLogInBox = true"
+                style="cursor: pointer"
               ></v-img>
-              <v-card-title>Logg inn</v-card-title>
+              <v-card-title @click="showLogInBox = true" style="cursor: pointer"
+                >Logg inn</v-card-title
+              >
             </v-card>
           </v-col>
           <v-col cols="4">
@@ -44,10 +52,15 @@
               <v-img
                 src="..\..\assets\Kort2.jpg"
                 class="white--text align-end"
-                @click="showRegisterBox = true"
                 height="280px"
+                @click="showRegisterBox = true"
+                style="cursor: pointer"
               ></v-img>
-              <v-card-title @click="showRegisterBox = true">Registrer deg</v-card-title>
+              <v-card-title
+                @click="showRegisterBox = true"
+                style="cursor: pointer"
+                >Registrer deg</v-card-title
+              >
             </v-card>
           </v-col>
           <v-col cols="4">
@@ -61,41 +74,33 @@
             </v-card>
           </v-col>
         </v-row>
-        <v-row class="justify-center">
-          <p v-if="!showFaq">Vis informasjon</p>
-          <p v-else>Lukk informasjon</p>
+        <v-row align="center" justify="center">
+          <p class="ma-0" v-if="!showFaq">Vis informasjon</p>
+          <p class="ma-0" v-else>Lukk informasjon</p>
         </v-row>
-        <v-row 
-          class="justify-center" 
-        >
-          <v-icon 
-            v-if="!showFaq"
-            x-large 
-            @click="atChevronClick"  
-          >
-            {{mdiChevronDown}}
-          </v-icon>
-          <v-icon 
-            v-else
-            x-large 
-            @click="atChevronClick"  
-          >
-            {{mdiChevronUp}}
-          </v-icon>
+        <v-row class="ma-0 justify-center">
+          <v-col cols="1">
+            <v-icon v-if="!showFaq" x-large @click="atChevronClick">
+              {{ mdiChevronDown }}
+            </v-icon>
+            <v-icon v-else x-large @click="atChevronClick">
+              {{ mdiChevronUp }}
+            </v-icon>
+          </v-col>
         </v-row>
         <v-row v-if="showFaq">
           <Information />
         </v-row>
-          <LoginDialog
-            @closeDialog="closeDialog"
-            @loggedIn="loggedIn"
-            :showDialog="showLogInBox"
-          />
-          <RegisterDialog
-            @closeDialog="closeDialog"
-            @loggedIn="loggedIn"
-            :showDialog="showRegisterBox"
-          />
+        <LoginDialog
+          @closeDialog="closeDialog"
+          @loggedIn="loggedIn"
+          :showDialog="showLogInBox"
+        />
+        <RegisterDialog
+          @closeDialog="closeDialog"
+          @loggedIn="loggedIn"
+          :showDialog="showRegisterBox"
+        />
       </v-col>
     </v-row>
   </v-container>
@@ -107,7 +112,7 @@ import Component from "vue-class-component";
 import LoginDialog from "../components/LoginDialog/LoginDialog.vue";
 import RegisterDialog from "../components/LoginDialog/RegisterDialog.vue";
 import Information from "../components/Users/Information.vue";
-import { mdiChevronDown, mdiChevronUp } from '@mdi/js';
+import { mdiChevronDown, mdiChevronUp } from "@mdi/js";
 
 @Component({
   components: {
