21cd524a845abe16a3c2f0d85b18889505e60014
Synnøve Sørensen <578010@stud.hvl.no>
2022-07-29 20:41:00 +0200

    Updated information page

--- clients/webclient/src/components/Users/Information.vue
+++ clients/webclient/src/components/Users/Information.vue
@@ -1,89 +1,117 @@
 <template>
   <v-container fluid>
     <v-row justify="center" dense>
-      <v-col >
-        <p class="headline my-8"> Spørsmål og svar </p>
-          <p class="font-weight-medium">Hvordan lager jeg abonnement?</p>
-          <p class="font-weight-light">
-            Du kan opprette et abonnement for å få leveringer på faste dager i
-            uken. Dette kan du gjøre ved å trykke på Registrer et abonnement på
-            siden for Brukerprofil. Så snart det er blitt verifisert at du bor
-            innenfor leveringsområdet til Lunsj på hjul, vil du motta faktura for
-            kommende periode. Når denne er betalt vil du kunne se dine leveranser
-            i kalenderen. Ta kontakt med Lunsj på Hjul dersom det er noe du lurer
-            på.
-          </p>
-          <p class="font-weight-medium">Hvor ofte får jeg faktura?</p>
-          <p class="font-weight-light">
-            Du faktureres én gang i måneden. Faktura sendes ut i løpet av den
-            siste uken i hver måned. Betalingsfrist er 1. i hver måned.
-          </p>
-          <p class="font-weight-medium">Hva skjer om jeg betaler for sent?</p>
-          <p class="font-weight-light">
-            Du vil ikke få noen leveranser før du har betalt fakturaen din. Dersom
-            du betaler for sent vil du likevel få antall leveranser du har betalt
-            for, men disse vil bli forskjøvet inn i neste periode, tilsvarende
-            hvor mye for sent du har betalt.
-          </p>
-          <p class="font-weight-medium">
-            Hva betyr de grønne og gule feltene i kalenderen?
-          </p>
-          <p class="font-weight-light">
-            De grønne feltene representerer dager du abonnerer på leveranser, og
-            som du har betalt for i kommende periode. De gule feltene
-            representerer dager Lunsj på Hjul tilbyr leveringer. Du kan bestille
-            ekstra leveranser ved å trykke på det gule feltet på en ønsket dato.
-            Informasjon om tidsfrist for bestilling kommer opp når du trykker på
-            feltet. Man kan bestille så mange leveranser man vil fra kalenderen,
-            enten man har abonnement eller ikke.
-          </p>
-          <p class="font-weight-medium">
-            Hvordan kan jeg avbestille en leveranse?
-          </p>
-          <p class="font-weight-light">
-            I kalenderen kan du avbestille leveranser, én og én. Du trykker på
-            leveransen du vil avbestille og velger så 'Kanseller'.
-          </p>
-          <p class="font-weight-medium">
-            Kan jeg sette abonnementet mitt på pause?
-          </p>
-          <p class="font-weight-light">
-            Du kan sette abonnement på pause ved å trykke 'Pause abonnement' i
-            feltet for abonnement på siden for Kundeprofil. Abonnementet vil
-            settes på pause i det du trykker på knappen, men du vil motta
-            leveranse dersom du trykker på pause senere enn klokken 10:00 dagen
-            før en leveranse.
-          </p>
-          <p class="font-weight-medium">Hvilke tidsfrister gjelder?</p>
-          <p class="font-weight-light">
-            Dersom du ønsker å kjøpe en leveranse, kansellere en leveranse eller
-            sette abonnementet må du gjøre dette før klokken ti dagen før
-            leveransen. Ønsker du å kansellere en leveranse torsdag, må du gjøre
-            dette før klokken 10:00 onsdag samme uke. Det samme gjelder for
-            bestilling.
-          </p>
-          <p class="font-weight-medium">Hvordan endrer jeg mitt abonnement?</p>
-          <p class="font-weight-light">
-            Du kan endre abonnementet ditt ved å trykke på blyantikonet øverst til
-            høyre i boksen for Abonnement på siden for Kundeprofil.
-          </p>
-          <p class="font-weight-medium">Hvordan endrer jeg mine opplysninger?</p>
-          <p class="font-weight-light">
-            Du kan endre opplysninger ved å trykke på blyantikonet øverst til
-            høyre i boksen for Kundeprofil på siden for Kundeprofil. Det er
-            dessverre ikke mulig å endre epostadresse uten å opprette en ny konto.
-          </p>
-          <p class="font-weight-medium">Kan jeg slette mine opplysninger?</p>
-          <p class="font-weight-light">
-            Du kan når som helst slette lagrede opplysninger, men dersom du gjør
-            dette mens du har utestående leveranser vil disse ikke kunne leveres
-            til deg. Det anbefales derfor å vente med å slette kundeprofilen til
-            du har mottatt den siste leveransen du har betalt for. <br />
-            Du kan slette dine opplysninger ved å først trykke på blyantikonet i
-            boksen for Kundeprofil, deretter på 'Slett alle data'.
-          </p>
+      <v-col>
+        <p class="headline my-8">Spørsmål og svar</p>
+        <p class="font-weight-medium">
+          Hvordan bestiller eg frå Lunsj på Hjul?
+        </p>
+        <p class="font-weight-light">
+          Først registerer du en kundeprofil ved å trykke på <strong>Registrer</strong> på
+          startsiden til lunsjpahjul.no. Leveringsadressen din må så verifiseres
+          av Anne Lill i Lunsj på Hjul for å sikre at du holder til innenfor
+          dekningsområdet. Dette vil skje så fort som mogleg, men mens du venter
+          kan du teikne deg et abonnement.
+        </p>
+        <p class="font-weight-medium">
+          Hvordan bestiller eg en enkeltlevering?
+        </p>
+        <p class="font-weight-light">
+          Når adressen har blitt verifisert så kan du bestille enkle leveringer
+          på bestemte dager utan å teikne eit abonnement. Da går du inn på
+          <strong>Kalender</strong> og finner dagen/dagene du vil ha levering. Dagene med gult
+          felt er dagene med levering som er mogleg å velje frå. Da bare klikker
+          du på den aktuelle dagen og klikker <strong>Bestill</strong> i menyen som kjem ved
+          sidan av/under kalenderen.
+        </p>
+        <p class="font-weight-medium">Hvordan lager jeg abonnement?</p>
+        <p class="font-weight-light">
+          Du kan opprette et abonnement for å få leveringer på faste dager i
+          uken. Dette kan du gjøre ved å trykke på <strong>Registrer et abonnement</strong> på
+          siden for <strong>Kundeprofil</strong>. Så snart Anne Lill har fått verifisert at du
+          bor innenfor leveringsområdet til Lunsj på hjul, vil du motta faktura
+          på epost for kommende periode. Når denne er betalt vil du kunne se
+          dine kommende leveranser i kalenderen. Disse vil være grønne. Ta
+          kontakt på lunsjpahjul@gmail.com dersom det er noe du lurer på.
+        </p>
+        <p class="font-weight-medium">
+          Hvor ofte får jeg faktura?
+        </p>
+        <p class="font-weight-light">
+          Du faktureres én gang i måneden der du forhåndsbetaler for kommende
+          kalendermåned. Faktura sendes ut den 22. i hver månad med
+          betalingsfrist den 1. påfølgende måned.
+        </p>
+        <p class="font-weight-medium">
+          Hva skjer om jeg betaler for sent?
+        </p>
+        <p class="font-weight-light">
+          Du vil ikke få noen leveranser før du har betalt fakturaen din. Dersom
+          du betaler for sent vil du likevel få antall leveranser du har betalt
+          for, men disse vil bli tilsvarende forskjøvet inn i neste periode.
+        </p>
+        <p class="font-weight-medium">
+          Hva betyr de grønne og gule feltene i kalenderen?
+        </p>
+        <p class="font-weight-light">
+          De grønne feltene representerer dager du abonnerer på leveranser, og
+          som du har betalt for i kommende periode. De gule feltene
+          representerer øvrige dager Lunsj på Hjul tilbyr leveringer. Du kan
+          bestille ekstra leveranser ved å trykke på det gule eller grønne
+          feltet på en ønsket dato. Informasjon om tidsfrist for bestilling
+          kommer opp når du trykker på feltet. Man kan bestille så mange
+          leveranser man vil fra kalenderen, enten man har abonnement eller
+          ikke.
+        </p>
+        <p class="font-weight-medium">Hvilke tidsfrister gjelder?</p>
+        <p class="font-weight-light">
+          Dersom du ønsker å kjøpe en leveranse, kansellere en leveranse eller
+          sette abonnementet på pause må du gjøre dette før klokken 10:00 dagen
+          før den aktuelle leveransen. Ønsker du altså å kansellere en leveranse
+          torsdag, må du gjøre dette før klokken 10:00 onsdag samme uke. Det
+          samme gjelder for bestilling og pause.
+        </p>
+        <p class="font-weight-medium">
+          Hvordan kan jeg avbestille en leveranse?
+        </p>
+        <p class="font-weight-light">
+          I kalenderen kan du avbestille leveranser, én og én. Du trykker på
+          leveransen du vil avbestille og velger så <strong>Kanseller</strong>. 
+          Dersom du angrer en kansellering så må du ta kontakt med Anne Lill 
+          for å få endret det tilbake.
+        </p>
+        <p class="font-weight-medium">
+          Kan jeg sette abonnementet mitt på pause?
+        </p>
+        <p class="font-weight-light">
+          Du kan sette abonnement på pause ved å trykke <strong>Pause abonnement</strong> i
+          feltet for abonnement på siden for <strong>Kundeprofil</strong>. Abonnementet vil
+          settes på pause i det du trykker på knappen, men du vil motta neste
+          leveranse dersom du trykker på pause senere enn klokken 10:00 dagen
+          før. Abonnementet vil være pauset helt til du selv aktiverer det igjen.
+        </p>
+        <p class="font-weight-medium">Hvordan endrer jeg mitt abonnement?</p>
+        <p class="font-weight-light">
+          Du kan endre abonnementet ditt ved å trykke på knappen <strong>Endre</strong> øverst til
+          høyre i boksen for Abonnement på siden for Kundeprofil.
+        </p>
+        <p class="font-weight-medium">Hvordan endrer jeg mine opplysninger?</p>
+        <p class="font-weight-light">
+          Du kan endre opplysninger ved å trykke på knappen <strong>Endre</strong> øverst til
+          høyre i boksen for Kundeprofil på siden for Kundeprofil. Det er
+          dessverre ikke mulig å endre epostadresse uten å opprette en ny konto.
+          Dersom du flytter og må endre adresse, kontakter du Anne Lill. 
+        </p>
+        <p class="font-weight-medium">Kan jeg slette mine opplysninger?</p>
+        <p class="font-weight-light">
+          Du kan når som helst slette lagrede opplysninger, men dersom du gjør
+          dette mens du har utestående leveranser vil disse ikke kunne leveres
+          til deg. Det anbefales derfor å vente med å slette kundeprofilen til
+          du har mottatt den siste leveransen du har betalt for. <br />
+          Du kan slette dine opplysninger ved å først trykke på <strong>Endre</strong> i
+          boksen for Kundeprofil, deretter på <strong>Slett alle data</strong>.
+        </p>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
